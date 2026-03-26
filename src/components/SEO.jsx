17878ba2b33fab09bf873964ec10ce3678aaf7a8
Subodh Mankala <subodhmankala@gmail.com>
2026-03-26 18:23:55 +0530

    - Added SEO of all the pages.
    - Updted the Index.html

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -7,12 +7,18 @@ const SEO = ({ title, description, name, type }) => {
             {/* Standard metadata tags */}
             <title>{title}</title>
             <meta name='description' content={description} />
-            {/* End standard metadata tags */}
-            {/* Facebook & Instagram tags (Open Graph) */}
+            <link rel="canonical" href={`https://fitengineerss.com${window.location.pathname}`} />
+            
+            {/* Open Graph / Facebook / Instagram */}
             <meta property='og:type' content={type} />
             <meta property='og:title' content={title} />
             <meta property='og:description' content={description} />
-            {/* End Facebook & Instagram tags */}
+            <meta property='og:url' content={`https://fitengineerss.com${window.location.pathname}`} />
+
+            {/* Twitter Card tags */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
         </Helmet>
     );
 };
